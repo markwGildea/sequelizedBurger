23b8317ee95215ff0be268931b1a52c50e2bf5e9
Mark Gildea <gildeamark1@gmail.com>
2017-02-04 09:29:46 -0600

    saturday

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "mgof5562",
   database: "burgers_db"
 });
 
